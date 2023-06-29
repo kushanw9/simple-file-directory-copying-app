be602446f457dc78367cdd969f1372e2a76f81af
Kushan Weerarathne <kushanweerarathne14@gmail.com>
2023-06-30 00:57:20 +0530

    Initial commit-UI design completed

---
+++ src/main/java/lk/ijse/dep10/filecopyapp/AppInitializer.java
@@ -0,0 +1,14 @@
+package lk.ijse.dep10.filecopyapp;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
---
+++ src/main/java/lk/ijse/dep10/filecopyapp/controller/CopySceneController.java
@@ -0,0 +1,64 @@
+package lk.ijse.dep10.filecopyapp.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+
+public class CopySceneController {
+
+    @FXML
+    private Button btnCopy;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnDestinationBrowse;
+
+    @FXML
+    private Button btnMove;
+
+    @FXML
+    private Button btnSourceBrowse;
+
+    @FXML
+    private Label lblPercentage;
+
+    @FXML
+    private ProgressBar prgBar;
+
+    @FXML
+    private TextField txtDestination;
+
+    @FXML
+    private TextField txtSource;
+
+    @FXML
+    void btnCopyOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDestinationBrowseOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnMoveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSourceBrowseOnAction(ActionEvent event) {
+
+    }
+
+}

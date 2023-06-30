3073c8b8a41afa5868d00962f4c069955be51c5e
Kushan Weerarathne <kushanweerarathne14@gmail.com>
2023-06-30 10:22:03 +0530

    Update some UI features

--- src/main/java/lk/ijse/dep10/filecopyapp/AppInitializer.java
+++ src/main/java/lk/ijse/dep10/filecopyapp/AppInitializer.java
@@ -1,6 +1,8 @@
 package lk.ijse.dep10.filecopyapp;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class AppInitializer extends Application {
@@ -8,7 +10,13 @@ public class AppInitializer extends Application {
         launch(args);
     }
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(new Scene(new FXMLLoader(getClass().getResource("/view/CopyScene.fxml")).load()));
+        primaryStage.setHeight(200);
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("File/Directory Handling App");
+        primaryStage.show();
+        primaryStage.centerOnScreen();
 
     }
 }
